439d8368b2e6c5fc6b8de14f37cc7feaaacd5cf2
Anton Galitsyn <anton.galitsyn@gmail.com>
2013-12-21 22:21:00 +0700

    new structure

--- install.sh
+++ install.sh
@@ -2,25 +2,33 @@
 which git > /dev/null || ( echo 'Git not found.' && exit )
 which easy_install > /dev/null || ( echo 'easy_install not found (install python-setuptools).' && exit )
 
+function link() {
+    local file
+    for path in $1/*; do
+        file=$(basename $path)
+
+        [ -f $HOME/.$file -a ! -h $HOME/.$file ] && mv $HOME/.$file $HOME/.$file.orig
+        [ ! -f $HOME/.$file ] && ln -vsf $dir/$path $HOME/.$file
+    done
+}
+
 # Link new files and backup old
-DIR=$PWD/`dirname $0`
+dir=$PWD/`dirname $0`
 cd `dirname $0`
 
-excluded=(install.sh README.md fonts.conf osx ubuntu_packages)
-for FILE in *; do
-	if [[ ! ${excluded[*]} =~ "$FILE" ]]; then
-		[ -f ~/.$FILE -a ! -h ~/.$FILE ] && mv ~/.$FILE ~/.$FILE.orig
-    	[ ! -f ~/.$FILE ] && ln -vsf $DIR/$FILE ~/.$FILE
-	fi
-done
+link 'bash'
+link 'vim'
+link 'git'
+link 'rc'
+link 'tools'
 
-cd ~
-[ ! -d ~/bin ] && mkdir ~/bin
+cd $HOME
+[ ! -d $HOME/bin ] && mkdir $HOME/bin
 
 # https://github.com/rupa/z
 if [ ! -e "$HOME/bin/z" ]; then
-    git clone https://github.com/rupa/z.git ~/bin/z
-    chmod +x ~/bin/z/z.sh
+    git clone https://github.com/rupa/z.git $HOME/bin/z
+    chmod +x $HOME/bin/z/z.sh
     # z binary is already referenced from .bash_profile
 fi
 
