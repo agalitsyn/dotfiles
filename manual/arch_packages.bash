33cff9e2a347c9d768befd7579513278b08df31d
Anton Galitsyn <anton.galitsyn@gmail.com>
2014-02-07 22:19:03 +0700

    add new packages

--- manual/arch_packages.bash
+++ manual/arch_packages.bash
@@ -14,14 +14,16 @@ systemctl enable ntpd.service
 updatedb
 alsamixter --unmute
 
-pacman -S --noconfirm xorg-server xorg-server-utils xorg-xinit mesa xorg-twm xorg-xclock xterm xclip xscreensaver numlockx xdm-archlinux
+pacman -S --noconfirm xorg-server xorg-server-utils xorg-xinit mesa xorg-twm xorg-xclock xterm xclip xscreensaver-arch-logo numlockx xdm-archlinux
 # Need to install video driver 
 # lspci | grep VGA
 # pacman -Ss xf86-video | less
 
 pacman -S --noconfirm awesome archlinux-wallpapper
 pacman -S --noconfirm ttf-droid ttf-dejavu terminus-font ttf-ubuntu-font-family
-pacman -S --noconfirm subversion htop tree wget tmux python-setuptools archey3 imagemagick smartmontools unrar fakeroot binutils
+pacman -S --noconfirm subversion htop tree wget tmux python-setuptools archey3 imagemagick smartmontools unrar fakeroot binutils make gcc ncurses
+pacman -S --noconfirm gvfs udisks2
+pacman -S --noconfirm mpd ncmpcpp mplayer
 
 pacman -S --noconfirm ranger libcaca highlight atool lynx poppler mediainfo transmission-cli
 ranger --copy-config=all
@@ -31,5 +33,4 @@ pacman -S --noconfirm claws-mail
 pacman -S --noconfirm pcmanfm
 pacman -S --noconfirm vlc
 pacman -S --noconfirm libreoffice
-pacman -S --noconfirm gvfs udisks2
 
