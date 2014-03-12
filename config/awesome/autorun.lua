53cf05799271ff72f2ab48de940d4de27bf940b9
Anton Galitsyn <anton.galitsyn@gmail.com>
2014-03-12 12:50:12 +0700

    add led fix for small keyboards

--- config/awesome/autorun.lua
+++ config/awesome/autorun.lua
@@ -1,5 +1,5 @@
 run_once("xrandr", "--output VGA-0 --mode 1920x1080 --right-of DVI-0") -- dual screens
-run_once("setxkbmap", "-option grp:switch,grp:alt_shift_toggle,grp_led:scroll,caps:none us,ru") -- keyboard
+run_once("setxkbmap", "-option grp:switch,grp:alt_shift_toggle,grp_led:caps,caps:none us,ru") -- keyboard
 run_once("xscreensaver", "-no-splash") -- starts screensaver daemon
 run_once("numlockx") -- turn numlock on
 -- run_once("unclutter") -- hide mouse cursor when it not used
