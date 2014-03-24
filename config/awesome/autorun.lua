7f0c0a8afa7d82418936bb667a545028e18c81f3
Anton Galitsyn <anton.galitsyn@gmail.com>
2014-03-24 18:27:02 +0700

    fix issue of pcmanfm tabs

--- config/awesome/autorun.lua
+++ config/awesome/autorun.lua
@@ -1,7 +1,20 @@
-run_once("xrandr", "--output VGA-0 --mode 1920x1080 --right-of DVI-0") -- dual screens
-run_once("setxkbmap", "-option grp:switch,grp:alt_shift_toggle,grp_led:caps,caps:none us,ru") -- keyboard
-run_once("xscreensaver", "-no-splash") -- starts screensaver daemon
-run_once("numlockx") -- turn numlock on
--- run_once("unclutter") -- hide mouse cursor when it not used
-run_once(os.getenv("HOME") .. "/.dropbox-dist/dropboxd") -- dropbox daemon
-run_once("wmname", "LG3D") -- java fix
+-- dual screens
+run_once("xrandr", "--output VGA-0 --mode 1920x1080 --right-of DVI-0")
+
+-- keyboard
+run_once("setxkbmap", "-option grp:switch,grp:alt_shift_toggle,grp_led:caps,caps:none us,ru")
+
+-- starts screensaver daemon
+run_once("xscreensaver", "-no-splash")
+
+-- turn numlock off because of http://sourceforge.net/p/pcmanfm/bugs/482/
+run_once("numlockx off")
+
+-- hide mouse cursor when it not used
+-- run_once("unclutter")
+
+-- dropbox daemon
+run_once(os.getenv("HOME") .. "/.dropbox-dist/dropboxd")
+
+-- java fix
+run_once("wmname", "LG3D")
