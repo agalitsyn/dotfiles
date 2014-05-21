98ffbd493379bdd54e33c7d660b728c3a87fc818
Anton Galitsyn <anton.galitsyn@gmail.com>
2014-05-21 13:07:55 +0700

    Fix bug

--- uninstall.sh
+++ uninstall.sh
@@ -6,7 +6,7 @@ echo 'Attention, it will destory all symlinks in' $HOME
 echo 'Are you sure? (y/n)'
 
 read proceed
-if [[ $proceed == "N" || $proceed == "n" ]]; then
+if [ "$proceed" = 'N' ] || [ "$proceed" = 'n' ]; then
     echo "Canceled."
 else
     find $HOME -maxdepth 1 -type l -exec rm -vf {} \;
