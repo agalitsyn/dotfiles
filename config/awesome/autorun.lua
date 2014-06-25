e809173df6f3f8f380717ec267b7f7503c6e5993
Anton Galitsyn <anton.galitsyn@gmail.com>
2014-06-25 11:46:48 +0700

    Add clipboard manager

--- config/awesome/autorun.lua
+++ config/awesome/autorun.lua
@@ -1,13 +1,18 @@
--- dual screens
+-- Screen
 run_once("xrandr", "--output VGA-0 --mode 1920x1080 --right-of DVI-0")
 
--- keyboard
+-- Keyboard
 run_once("setxkbmap", "-option grp:switch,grp:alt_shift_toggle,grp_led:caps,caps:none us,ru")
+
 -- this will make Caps Lock to act as Esc
 run_once("xmodmap", "-e 'keycode 66 = Escape NoSymbol Escape'")
+
 -- turn numlock on
-run_once("numlockx")
 -- pcmanfm have bug with numlock, see http://sourceforge.net/p/pcmanfm/bugs/482/
+run_once("numlockx")
+
+-- clipboard manager
+run_once("parcellite")
 
 -- starts screensaver daemon
 run_once("xscreensaver", "-no-splash")
@@ -19,4 +24,4 @@ run_once("unclutter")
 run_once(os.getenv("HOME") .. "/.dropbox-dist/dropboxd")
 
 -- java fix
-run_once("wmname", "LG3D")
+-- run_once("wmname", "LG3D")
