55bf3c47a4da0c246dc723b77f3bdb9187c21fa4
Anton Galitsyn <anton.galitsyn@gmail.com>
2014-12-10 13:13:48 +0600

    Update osx bootstrapper

--- bootstrap/01-bootstrap-osx.bash
+++ bootstrap/01-bootstrap-osx.bash
@@ -52,29 +52,58 @@ brew install node # This installs `npm` too using the recommended installation m
 brew cleanup
 
 echo 'Manually install apps:'
+
+echo '==> Terminal'
 echo 'Iterm 2:'
 open https://iterm2.com/downloads.html
+
+echo '==> Keyboard'
 echo 'Spectacle:'
 open http://spectacleapp.com/
 echo 'Flycut:'
 open https://itunes.apple.com/ru/app/flycut-clipboard-manager/id442160987?mt=12
+echo 'Seil (Keyboard hacks)'
+open https://pqrs.org/osx/karabiner/seil.html.en
+
+echo '==> Monitoring tools'
+echo 'Menu meters'
+open http://www.ragingmenace.com/software/menumeters/#download
+
+echo '==> Browsers'
 echo 'Firefox:'
 open http://www.mozilla.org/en-US/firefox/new/
 echo 'Chrome:'
 open https://www.google.com/intl/en/chrome/browser/
+
+echo '==> Editors'
 echo 'Sublime text:'
 open http://www.sublimetext.com/3
+echo 'Pycharm'
+open https://www.jetbrains.com/pycharm/download
+
+echo '==> Cloud data storages'
 echo 'Dropbox:'
 open https://www.dropbox.com
+
+echo '==> Messaging'
 echo 'Skype:'
 open http://www.skype.com/en/download-skype/skype-for-computer/
 echo 'Hipchat:'
 open https://www.hipchat.com/downloads
+
+echo '==> Torrents'
 echo 'Transmission:'
 open http://www.transmissionbt.com
+
+echo '==> Media'
 echo 'VLC:'
 open http://www.videolan.org/vlc/index.html
-echo 'Virtualbox:'
+echo 'Popcorn time'
+open https://popcorntime.io/
+
+echo '==> Virtualization'
+echo 'Virtualbox and extensions:'
 open https://www.virtualbox.org/wiki/Downloads
 echo 'Vagrant:'
 open https://www.vagrantup.com/downloads
+
