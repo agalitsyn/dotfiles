54d5b4bdb8c375fd623e195c3fecd42270a388a1
Anton Galitsyn <anton.galitsyn@gmail.com>
2014-12-15 15:08:45 +0600

    update defaults

--- bootstrap/02-python-packages.sh
+++ bootstrap/02-python-packages.sh
@@ -5,5 +5,7 @@ sudo -v
 
 which pip > /dev/null || echo "pip not found" && exit 1
 
-sudo pip install pyflakes pep8 pylint pep257 virtualenv mccabe
+# Essentials
+sudo pip install ipython pyflakes pep8 pylint pep257 virtualenv
+# Sphinx ans others
 sudo pip install Pygments Sphinx sphinxcontrib-seqdiag sphinxcontrib-blockdiag
