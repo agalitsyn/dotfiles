9a741668034593d4d7b5307d7019270ed53f6e6a
Anton Galitsyn <anton.galitsyn@gmail.com>
2016-02-15 15:39:26 +0600

    sync dotfiles

--- config/i3/autostart.sh
+++ config/i3/autostart.sh
@@ -2,13 +2,16 @@
 
 xrandr --output eDP1 --mode 1600x900 --rotate normal --primary
 xrandr --output HDMI1 --mode 1920x1200 --above eDP1
-feh --bg-scale ~/.config/i3/owl.jpg
+feh --bg-scale ~/.config/i3/bg.jpg
 
 # See man xkeyboard-config
 setxkbmap -option grp:switch,grp:alt_space_toggle,grp_led:caps,caps:escape us,ru
 #xmodmap -e 'keycode 66 = Escape NoSymbol Escape' &
 numlockx &
 unclutter &
+fbxkb &
+
+dunst -config ~/.config/dunst/dunstrc &
 
 xautolock -time 5 -locker 'i3lock --nofork --dpms --color 000000' &
 pulseaudio --start &
