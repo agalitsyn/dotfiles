d29aa3b19fe1e9141ed0cd596afb9ed2715f5e95
Anton Galitsyn <anton.galitsyn@gmail.com>
2016-03-28 09:26:27 +0600

    sync repo

--- config/i3/autostart.sh
+++ config/i3/autostart.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 xrandr --output eDP1 --mode 1600x900 --rotate normal --primary
-xrandr --output HDMI1 --mode 1920x1200 --above eDP1
+xrandr --output HDMI1 --mode 1920x1200 --left-of eDP1
 feh --bg-scale ~/.config/i3/bg.jpg
 
 # See man xkeyboard-config
-setxkbmap -option grp:switch,grp:alt_space_toggle,grp_led:caps,caps:escape us,ru
+setxkbmap -option grp:switch,grp:alt_shift_toggle,grp_led:caps,caps:escape us,ru
 #xmodmap -e 'keycode 66 = Escape NoSymbol Escape' &
 numlockx &
 unclutter &
