cc0d87540556605423613f3da105ddd8a24969d9
Anton Galitsyn <anton.galitsyn@gmail.com>
2016-04-04 13:53:26 +0600

    sync i3 settings

--- config/i3/autostart.sh
+++ config/i3/autostart.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
-xrandr --output eDP1 --mode 1600x900 --rotate normal --primary
-xrandr --output HDMI1 --mode 1920x1200 --left-of eDP1
+xrandr \
+    --output eDP1 --mode 1600x900 --rotate normal --primary \
+    --output HDMI1 --off \
+    --output VGA1 --off \
+    --output DP1 --off
+
 feh --bg-scale ~/.config/i3/bg.jpg
 
 # See man xkeyboard-config
