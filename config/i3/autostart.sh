d5582222d3cbe4b3c1f8badb6ac630324b47bb64
Anton Galitsyn <anton.galitsyn@gmail.com>
2016-05-25 10:40:28 +0600

    sync

--- config/i3/autostart.sh
+++ config/i3/autostart.sh
@@ -11,9 +11,12 @@ feh --bg-scale ~/.config/i3/bg.jpg
 # See man xkeyboard-config
 setxkbmap -option grp:switch,grp:alt_shift_toggle,grp_led:caps,caps:escape us,ru
 #xmodmap -e 'keycode 66 = Escape NoSymbol Escape' &
+
 numlockx &
 unclutter &
 fbxkb &
+
+redshift -l 54.980645:82.895525 -t 6500:5000 -b 1.0:0.8 &
 
 dunst -config ~/.config/dunst/dunstrc &
 
