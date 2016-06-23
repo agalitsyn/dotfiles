7ced582378bd5af92acd63428740d7e2033e1df2
Anton Galitsyn <anton.galitsyn@gmail.com>
2016-06-23 22:00:20 +0600

    add more backported packages

--- bootstrap/debian/02-upgrade-from-backports.sh
+++ bootstrap/debian/02-upgrade-from-backports.sh
@@ -2,10 +2,12 @@
 
 set -xe
 
-apt-get install --verbose-versions --yes --no-install-recommends --target-release, jessie-backports \
+apt-get install --verbose-versions --yes --no-install-recommends --target-release jessie-backports \
 	linux-image-amd64 \
 	linux-headers-amd64 \
 	dkms \
+	qemu-system-x86 \
+	qemu-utils \
 	virtualbox \
 	xserver-xorg-video-intel \
 	firmware-linux-free \
