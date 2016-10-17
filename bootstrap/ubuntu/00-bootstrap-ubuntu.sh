7bff0bfa5b8b58a5c10f5dede1d09c30b55cbfb6
Anton Galitsyn <anton.galitsyn@gmail.com>
2016-10-17 12:15:31 +0700

    add clipboard tool for ubuntu

--- bootstrap/ubuntu/00-bootstrap-ubuntu.sh
+++ bootstrap/ubuntu/00-bootstrap-ubuntu.sh
@@ -87,8 +87,8 @@ apt-get install -y --no-install-recommends \
 	ctags \
 	git \
 	gitk \
-	shellcheck
-
+	shellcheck \
+	xsel
 update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
 update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
 update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
