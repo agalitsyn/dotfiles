6635a3212208eeea39a431dde0c342f9c01b4336
Anton Galitsyn <agalitsyn@users.noreply.github.com>
2017-10-25 18:49:20 +0700

    update

--- vscode/install.sh
+++ vscode/install.sh
@@ -2,6 +2,8 @@
 
 set -xe
 
+code --install-extension ms-vscode.sublime-keybindings
+code --install-extension ms-vscode.cpptools
 code --install-extension EditorConfig.EditorConfig
 code --install-extension HookyQR.beautify
 code --install-extension christian-kohler.path-intellisense
@@ -9,13 +11,10 @@ code --install-extension dbaeumer.vscode-eslint
 code --install-extension donjayamanne.python
 code --install-extension formulahendry.auto-close-tag
 code --install-extension formulahendry.auto-rename-tag
-code --install-extension formulahendry.code-runner
 code --install-extension lukehoban.Go
-code --install-extension ms-vscode.sublime-keybindings
 code --install-extension msjsdiag.debugger-for-chrome
 code --install-extension steve8708.Align
 code --install-extension wayou.vscode-todo-highlight
-code --install-extension ms-vscode.cpptools
 code --install-extension PeterJausovec.vscode-docker
 code --install-extension tonsky.theme-alabaster
 code --install-extension PKief.material-icon-theme
