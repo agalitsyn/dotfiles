b026b9fa576d1f8f3b5aebce21dfc745f05a007f
Anton Galitsyn <agalitsyn@users.noreply.github.com>
2018-02-28 10:38:12 +0700

    update

--- vscode/install.sh
+++ vscode/install.sh
@@ -5,3 +5,8 @@ set -xe
 code --install-extension ms-vscode.cpptools
 code --install-extension EditorConfig.EditorConfig
 code --install-extension lukehoban.Go
+code --install-extension ms-python.python
+code --install-extension wayou.vscode-todo-highlight
+code --install-extension PeterJausovec.vscode-docker
+code --install-extension PKief.material-icon-theme
+
