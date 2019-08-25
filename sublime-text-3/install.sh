214dd67f35cd166f8f9907bd525c921f44d0d636
Anton Galitsyn <agalitsyn@users.noreply.github.com>
2019-08-25 15:10:50 +0700

    update

--- sublime-text-3/install.sh
+++ sublime-text-3/install.sh
@@ -3,7 +3,7 @@
 set -xe
 
 CONFIG=$HOME/.config/sublime-text-3
-[[ "$OSTYPE" == "linux-gnu" ]] || CONFIG=$HOME/Library/Application Support/Sublime\ Text\ 3/
+[[ "$OSTYPE" == "linux-gnu" ]] || CONFIG=$HOME/Library/Application\ Support/Sublime\ Text\ 3/
 
 mkdir -p "$CONFIG/Packages/User"
 cp config/* "$CONFIG/Packages/User"
