96d7c041362ecf2290b0e02bd986dbf46f1e2620
Anton Galitsyn <agalitsyn@users.noreply.github.com>
2020-09-01 16:29:13 +0700

    update

---
+++ vscode/vscode.zsh
@@ -0,0 +1,4 @@
+# see https://github.com/Microsoft/vscode/issues/48480
+# may be update https://github.com/Microsoft/vscode/blob/master/resources/linux/code.desktop
+alias code="GTK_IM_MODULE='xim' code $@"
+
