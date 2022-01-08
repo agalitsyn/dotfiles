f934d75bb4565e1df6d2f79888cb9d9de8293130
Anton Galitsyn <anton.galitsyn@gmail.com>
2022-01-09 01:35:58 +0700

    use p10k

--- sublime-text/install.sh
+++ sublime-text/install.sh
@@ -2,9 +2,10 @@
 
 set -xe
 
-CONFIG=$HOME/.config/sublime-text-3
+CONFIG=$HOME/.config/sublime-text
 [[ "$OSTYPE" == "linux-gnu" ]] || CONFIG=$HOME/Library/Application\ Support/Sublime\ Text
 
 mkdir -p "$CONFIG/Packages/User"
 # copy instead of linking because sublime reformats it and delete all the comments
 cp config/* "$CONFIG/Packages/User"
+
