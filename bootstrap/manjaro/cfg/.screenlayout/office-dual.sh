f7d4abefcba282b9f31bd80e157ee32f81a8cf0c
Anton Galitsyn <agalitsyn@ptsecurity.com>
2022-11-12 00:00:15 +0700

    update

--- bootstrap/manjaro/cfg/.screenlayout/office-dual.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-xrandr --output eDP-1 --mode 1920x1080 --pos 0x360 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --primary --mode 2560x1440 --pos 1920x0 --rotate normal --output DP-3 --off --output DP-4 --off --output DP-5 --off --output HDMI-1-0 --off
-sleep 1
-setxkbmap -option grp:switch,grp:alt_shift_toggle,grp_led:caps,caps:escape us,ru
-nitrogen --restore
-
