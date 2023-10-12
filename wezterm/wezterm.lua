0cf61341104742ca277ceb98b8f3ec2a408c582f
Anton Galitsyn <anton.galitsyn@gmail.com>
2023-10-12 10:19:02 +0700

    update

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,60 @@
+local wezterm = require 'wezterm'
+
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+
+config.color_scheme = 'Solarized (dark) (terminal.sexy)'
+
+config.font_size = 15
+config.font = wezterm.font {
+  family = 'JetBrains Mono',
+}
+
+config.window_frame = {
+  -- The font used in the tab bar.
+  -- Roboto Bold is the default; this font is bundled
+  -- with wezterm.
+  -- Whatever font is selected here, it will have the
+  -- main font setting appended to it to pick up any
+  -- fallback fonts you may have used there.
+  font = wezterm.font { family = 'Roboto', weight = 'Bold' },
+
+  -- The size of the font in the tab bar.
+  -- Default to 10.0 on Windows but 12.0 on other systems
+  font_size = 12.0,
+
+  -- The overall background color of the tab bar when
+  -- the window is focused
+  active_titlebar_bg = '#333333',
+
+  -- The overall background color of the tab bar when
+  -- the window is not focused
+  inactive_titlebar_bg = '#333333',
+}
+
+config.colors = {
+  tab_bar = {
+    -- The color of the inactive tab bar edge/divider
+    inactive_tab_edge = '#575757',
+  },
+}
+
+-- Enable the scrollbar.
+-- It will occupy the right window padding space.
+-- If right padding is set to 0 then it will be increased
+-- to a single cell width
+config.enable_scroll_bar = true
+
+-- How many lines of scrollback you want to retain per tab
+config.scrollback_lines = 20000
+
+-- default keybindings https://wezfurlong.org/wezterm/config/default-keys.html?h=def
+
+return config
+
