b31569ca2e35d97cf79f220a3b099ac0fd7a129e
Anton Galitsyn <anton.galitsyn@gmail.com>
2023-11-13 13:41:54 +0700

    update

--- i3/i3/scripts/display-preset-switcher.sh
+++ i3/i3/scripts/display-preset-switcher.sh
@@ -14,7 +14,7 @@ case "$1" in
         xrandr --output eDP-1 --off --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --off --output DP-5 --off --output HDMI-1-0 --primary --mode 3440x1440 --pos 0x0 --rotate normal
         ;;
     office-dual)
-        xrandr --output eDP-1 --mode 1920x1080 --pos 0x360 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --primary --mode 2560x1440 --pos 1920x0 --rotate normal --output DP-3 --off --output DP-4 --off --output DP-5 --off --output HDMI-1-0 --off
+		xrandr --output eDP-1 --mode 1920x1080 --pos 2560x360 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP-3 --off --output DP-4 --off --output DP-5 --off --output HDMI-1-0 --off
         ;;
     office-single)
         xrandr --output eDP-1 --off --output DP-1 --off --output HDMI-1 --off --output DP-2 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP-3 --off --output DP-4 --off --output DP-5 --off --output HDMI-1-0 --off
