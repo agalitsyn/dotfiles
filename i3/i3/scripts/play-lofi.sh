c3033cce69c8e5bf8f9ad5feceea20a63ad4298b
Anton Galitsyn <anton.galitsyn@gmail.com>
2023-11-24 10:11:05 +0700

    update

--- i3/i3/scripts/play-lofi.sh
+++ i3/i3/scripts/play-lofi.sh
@@ -1,2 +1,4 @@
+# for graphical approach see https://github.com/raphtlw/dotfiles/blob/master/bin/ytplay
+
 URL=$(youtube-dl -f best --get-url 'https://www.youtube.com/watch?v=jfKfPfyJRdk')
 ffplay -autoexit -hide_banner -loglevel error -nodisp -volume 100 "${URL}"
