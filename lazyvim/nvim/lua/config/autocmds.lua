4e7e84f8dfbb898333f869d322f4024833004013
Anton Galitsyn <anton.galitsyn@gmail.com>
2024-04-16 16:55:02 +0700

    always use english keymap

--- lazyvim/nvim/lua/config/autocmds.lua
+++ lazyvim/nvim/lua/config/autocmds.lua
@@ -1,3 +1,11 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Always switch to English after insert mode
+vim.api.nvim_create_autocmd({ "InsertLeave" }, {
+  pattern = "*",
+  callback = function()
+    vim.fn.system({ "im-select", "com.apple.keylayout.US" })
+  end,
+})
