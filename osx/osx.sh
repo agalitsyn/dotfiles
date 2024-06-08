5c587335c5e85914650d137e05c9171cdf722583
Anton Galitsyn <anton.galitsyn@gmail.com>
2024-06-08 14:15:17 +0700

    finish zsh setup

--- osx/osx.sh
+++ osx/osx.sh
@@ -1,26 +1,25 @@
 eval $(/opt/homebrew/bin/brew shellenv)
-
 export HOMEBREW_NO_AUTO_UPDATE=true
 
 # link all bins
-export PATH="/opt/homebrew/sbin:$PATH"
-export PATH="/opt/homebrew/bin:$PATH"
+export PATH="$HOMEBREW_PREFIX/sbin:$PATH"
+export PATH="$HOMEBREW_PREFIX/bin:$PATH"
 
 # link python3 as python
-export PATH="/opt/homebrew/opt/python/libexec/bin:$PATH"
+export PATH="$HOMEBREW_PREFIX/opt/python/libexec/bin:$PATH"
 
 # substitute man pages
-export PATH="/opt/homebrew/opt/coreutils/libexec/gnubin:$PATH"
-export MANPATH="/opt/homebrew/opt/coreutils/libexec/gnuman:$MANPATH"
+export PATH="$HOMEBREW_PREFIX/opt/coreutils/libexec/gnubin:$PATH"
+export MANPATH="$HOMEBREW_PREFIX/opt/coreutils/libexec/gnuman:$MANPATH"
 
-export PATH="/opt/homebrew/opt/curl/bin:$PATH"
+export PATH="$HOMEBREW_PREFIX/opt/curl/bin:$PATH"
 
-export PKG_CONFIG_PATH="/opt/homebrew/opt/openssl/lib/pkgconfig"
-export LDFLAGS="-I/opt/homebrew/opt/openssl/include -L/opt/homebrew/opt/openssl/lib"
+export PKG_CONFIG_PATH="$HOMEBREW_PREFIX/opt/openssl/lib/pkgconfig"
+export LDFLAGS="-I$HOMEBREW_PREFIX/opt/openssl/include -L$HOMEBREW_PREFIX/opt/openssl/lib"
 
-export PATH="/opt/homebrew/opt/libpq/bin:$PATH"
+export PATH="$HOMEBREW_PREFIX/opt/libpq/bin:$PATH"
 
-export PATH="/opt/homebrew/opt/openjdk/bin:$PATH"
+export PATH="$HOMEBREW_PREFIX/opt/openjdk/bin:$PATH"
 
 export PATH=~/.local/bin:$PATH
 
@@ -33,8 +32,8 @@ alias apache2-stop="sudo apachectl -k stop"
 alias apache2-restart="sudo apachectl -k restart"
 
 # postgres from homebrew
-alias postgresql-start="pg_ctl -D /opt/homebrew/var/postgres start"
-alias postgresql-stop="pg_ctl -D /opt/homebrew/var/postgres stop"
+alias postgresql-start="pg_ctl -D $HOMEBREW_PREFIX/var/postgres start"
+alias postgresql-stop="pg_ctl -D $HOMEBREW_PREFIX/var/postgres stop"
 
 # memcached from homebrew
 alias memcached-start="memcached -vv"
@@ -42,8 +41,8 @@ alias memcached-statistics="memstat --servers=127.0.0.1:11211"
 alias memcached-dump="memdump --servers=127.0.0.1:11211"
 
 # cd into whatever is the forefront Finder window.
-function cdf() {  # short for cdfinder
-  cd "`osascript -e 'tell app "Finder" to POSIX path of (insertion location as alias)'`"
+function cdf() { # short for cdfinder
+    cd "$(osascript -e 'tell app "Finder" to POSIX path of (insertion location as alias)')"
 }
 
 function anybar {
@@ -55,7 +54,7 @@ function ss {
 }
 
 function get_os_theme {
-    if [[ "$(defaults read -g AppleInterfaceStyle 2>/dev/null)"  == "Dark" ]]; then
+    if [[ "$(defaults read -g AppleInterfaceStyle 2>/dev/null)" == "Dark" ]]; then
         export OS_THEME='dark'
         export DELTA_FEATURES="+dark-mode"
     else
@@ -73,10 +72,11 @@ ITERM_SSH_PROFILE='FancySSH'
 # On iTerm we switch terminals for SSH if we have it.  This switches to
 # the SSH profile and back when ssh is run from the terminal.
 if [[ "$TERM_PROGRAM" == iTerm.app ]]; then
-  function ssh() {
-    echo -n -e $'\033]50;SetProfile='$ITERM_SSH_PROFILE'\a'
-    command ssh "$@"
-    echo -n -e $'\033]50;SetProfile='$ITERM_NORMAL_PROFILE'\a'
-  }
+    function ssh() {
+        echo -n -e $'\033]50;SetProfile='$ITERM_SSH_PROFILE'\a'
+        command ssh "$@"
+        echo -n -e $'\033]50;SetProfile='$ITERM_NORMAL_PROFILE'\a'
+    }
 fi
 
+source $HOMEBREW_PREFIX/etc/profile.d/z.sh
