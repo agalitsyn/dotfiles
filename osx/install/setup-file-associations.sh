ed40d24fc1fa906b947c7f19534072b79df60aaf
Anton Galitsyn <anton.galitsyn@gmail.com>
2024-06-28 14:24:13 +0700

    update

--- osx/install/setup-file-associations.sh
+++ osx/install/setup-file-associations.sh
@@ -11,16 +11,21 @@ trap "exit 1" TERM
 export TERM_PID=$$
 
 # check `duti` installed
-command -v duti >/dev/null 2>&1 || \
-  { echo >&2 "duti required: brew install duti"; exit 1; }
+command -v duti >/dev/null 2>&1 ||
+    {
+        echo >&2 "duti required: brew install duti"
+        exit 1
+    }
 
 get_bundle_id() {
-    osascript -e "id of app \"${1}\"" || kill -s TERM $TERM_PID;
+    osascript -e "id of app \"${1}\"" || kill -s TERM $TERM_PID
 }
 
 assoc() {
-    bundle_id=$1; shift
-    role=$1; shift
+    bundle_id=$1
+    shift
+    role=$1
+    shift
     while [ -n "$1" ]; do
         echo "setting file assoc: $bundle_id .$1 $role"
         duti -s "$bundle_id" ".${1}" "$role"
@@ -31,7 +36,6 @@ assoc() {
 TEXT_EDITOR=$(get_bundle_id "Sublime Text")
 MEDIA_PLAYER=$(get_bundle_id "VLC")
 
-assoc "$TEXT_EDITOR" "editor" txt md js jse json reg bat ps1 cfg sh bash yaml py ts go java
+assoc "$TEXT_EDITOR" "editor" txt md js jse json reg bat ps1 cfg sh bash yaml py ts tsx vue go java xml xsd
 assoc "$MEDIA_PLAYER" "viewer" mkv mp4 avi mov webm
 assoc "$MEDIA_PLAYER" "viewer" flac fla ape wav mp3 wma m4a ogg ac3
-
