7b1e13320afc3d5a3020d234b2b3119688f4c8a2
Anton Galitsyn <anton.galitsyn@gmail.com>
2026-02-11 14:37:20 +0700

    update

---
+++ claude/ai.sh
@@ -0,0 +1,7 @@
+export OPENAI_API_KEY=''                                                      │    │
+export ANTHROPIC_API_KEY=''                                                   │    │
+export DEEPSEEK_API_KEY=''                                                    │    │
+export GEMINI_API_KEY=''                                                      │    │
+export OPENROUTER_API_KEY=''                                                  │    │
+export BRAVE_API_KEY=''
+
